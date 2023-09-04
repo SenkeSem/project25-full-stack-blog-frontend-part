f387651f558147cf3323174e908a77d568020d52
SenkeSem <sen9morf@gmail.com>
2023-09-04 23:03:08 +0300

    v1.0 frontend under development

---
+++ src/pages/FullPost.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "../axios";
+
+import { Post } from "../components/Post";
+import { Index } from "../components/AddComment";
+import { CommentsBlock } from "../components/CommentsBlock";
+
+export const FullPost = () => {
+  const [data, setData] = React.useState();
+  const [isLoading, setLoading] = React.useState(true);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    axios
+    .get(`/posts/${id}`)
+    .then((res) => {
+      setData(res.data);
+      setLoading(false);
+    })
+    .catch((err) => {
+      console.log(err);
+      alert('Ошибка при получении статьи!');
+    });
+  }, []);
+
+  if (isLoading) {
+    return <Post isLoading={isLoading} isFullPost />;
+  }
+
+
+  return (
+    <>
+      <Post
+        _id={data._id}
+        title={data.title}
+        imageUrl="https://koshka.top/uploads/posts/2021-12/1640116763_1-koshka-top-p-na-rabochii-stol-koshki-1.jpg"
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
+        commentsCount={3}
+        tags={data.tags}
+        isFullPost
+      >
+        <p>
+          {data.text}
+        </p>
+      </Post>
+      <CommentsBlock
+        items={[
+          {
+            user: {
+              fullName: "Вася Пупкин",
+              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+            },
+            text: "Это тестовый комментарий 555555",
+          },
+          {
+            user: {
+              fullName: "Иван Иванов",
+              avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+            },
+            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+          },
+        ]}
+        isLoading={false}
+      >
+        <Index />
+      </CommentsBlock>
+    </>
+  );
+};
